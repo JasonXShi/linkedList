1aba0f308e0eed5cb24de0a97841eaabbc385233
Jason Shi <jasonxshi2002@gmail.com>
2017-12-20 08:30:00 -0800

    oops comments

--- Student.h
+++ Student.h
@@ -1,3 +1,6 @@
+//header files for students
+
+//header guards and importing iostream
 #ifndef STUDENT_H
 #define STUDENT_H
 #include <iostream>
@@ -5,6 +8,7 @@
 using namespace std;
 
 class Student{
+    //just makes a public constructor so we can test if adding a student to a node works
     public:
         Student();
 };
