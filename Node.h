358d0b2b736fbada1a39ae9c4d0213f0bc0e66ce
Jason Shi <jasonxshi2002@gmail.com>
2017-12-20 08:28:32 -0800

    oops comments

--- Node.h
+++ Node.h
@@ -1,14 +1,20 @@
+//this is the header file for a node
+
+//header guards and importing stuff
 #ifndef NODE_H
 #define NODE_H
 #include <iostream>
 #include "Student.h"
 
 using namespace std;
-
+//defines the public and private functions and variables
 class Node{
     public:
+        //constructor
         Node(Student* newStudent);
+        //destructor
         ~Node();
+        //makes setters and getters
         void setStudent(Student* newStudent);
         Student* getStudent();
         void setNext(Node* newNode);
@@ -16,6 +22,7 @@ class Node{
         void setLast(Node* newNode);
         Node* getLast();
     private:
+        //intializes the variables
         Student* student;
         Node* next;
         Node* last;
